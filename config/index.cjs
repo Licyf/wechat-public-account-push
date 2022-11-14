db32f1ada3f034e9071cf7308368be80d18e3c26
Licyf <118189561+Licyf@users.noreply.github.com>
2022-11-14 22:45:49 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'optsX6D5Js2sW5ISxALUhWnoB6bQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'QJI6aa9VF2UPNrtV0X1D4RiyWv6EYE0RLXEnECVaReQ',
+      useTemplateId: '	Qeq4U7fE31RgGH05PiZERGhlfB3hRKTgTKE4aNSrjok',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-25',
       festivals: [
