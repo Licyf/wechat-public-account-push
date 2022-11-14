8db67bceae9d61b13b127ae41375cdb9d4826318
Licyf <118189561+Licyf@users.noreply.github.com>
2022-11-14 22:31:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小凤宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'optsX6D5Js2sW5ISxALUhWnoB6bQ','optsX6EE-ZnD7NpDfWgXjjeXs2YM'
+      id: 'optsX6D5Js2sW5ISxALUhWnoB6bQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'tAwaUQfPszimPFAEjPq02GF4bNPIgRS3qyt-7IWmn3o',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'tAwaUQfPszimPFAEjPq02GF4bNPIgRS3qyt-7IWmn3o',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'optsX6EE-ZnD7NpDfWgXjjeXs2YM',
+      id: '',
     }
   ],
 
